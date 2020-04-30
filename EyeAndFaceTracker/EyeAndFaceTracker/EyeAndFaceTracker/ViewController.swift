0bdbd5ea485dd5497845841bf588f2f77b0e25df
SugarpineSoftware <corygreen84@hotmail.com>
2020-04-29 20:54:30 -0700

    Restart of the project

---
+++ EyeAndFaceTracker/EyeAndFaceTracker/EyeAndFaceTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeAndFaceTracker
+//
+//  Created by Cory Green on 4/29/20.
+//  Copyright © 2020 Cory Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
