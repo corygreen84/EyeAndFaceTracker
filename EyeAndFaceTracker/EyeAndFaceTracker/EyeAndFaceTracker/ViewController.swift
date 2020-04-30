4c56e9ee221a22cb081370c09ea8b515fecfa9ce
SugarpineSoftware <corygreen84@hotmail.com>
2020-04-29 21:50:38 -0700

    Starting in on capturing images

--- EyeAndFaceTracker/EyeAndFaceTracker/EyeAndFaceTracker/ViewController.swift
+++ EyeAndFaceTracker/EyeAndFaceTracker/EyeAndFaceTracker/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainCameraView: UIView!
+    
+    var session: AVCaptureSession?
+    var input: AVCaptureDeviceInput?
+    var output: AVCapturePhotoOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func getDevice(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        
+        /*
+        let devices: NSArray = AVCaptureDevice.DiscoverySession
+        
+        for d in devices {
+            let deviceConverted = d as! AVCaptureDevice
+        }
+        */
+        
+        return nil
     }
 
 
